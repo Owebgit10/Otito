e403d52ff426dde3d5f178de00e21ab05f8b1ea6
Owebgit10 <145547062+Owebgit10@users.noreply.github.com>
2023-10-09 12:01:36 +0100

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,86 +1,61 @@
-
 #define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
-
-
-
-#define MAX_SIZE 20 / *Maximum size of the arr can use #define MAX twenty also */
 
 
 
 void main() {
 
-    char arr[MAX_SIZE]; // Declare the character array
-
-    int i, j;
 
 
-
-    // Ask the user to input characters until a full stop is entered or the array is full
-
-    printf("Enter characters (maximum of %d characters, end with a full stop): ", MAX_SIZE);
-
-    for (i = 0; i < MAX_SIZE; i++) {
-
-        scanf(" %c", &arr[i]); // Read each character from the user
-
-        if (arr[i] == '.') break; // Stop if a full stop is entered
-
-    }
-
-    arr[i] = '\0'; // Add null terminator to end the string
+    /* Declaring the three integers that the users
 
 
 
-    // Display the entered characters
+    input will be read into */
 
-    printf("Entered characters: ");
+    int num1, num2, num3;
 
-    for (j = 0; arr[j] != '\0'; j++) {
-
-        printf("%c", arr[j]); // Print each character in the array
-
-    }
-
-    printf("\n");
+    float average;
 
 
 
-    // Display the vowels in the entered characters
 
-    printf("Vowels: ");
 
-    for (j = 0; arr[j] != '\0'; j++) {
+    //Asking the user to input the integers 
 
-        switch (arr[j]) { // Check each character for a vowel
-
-        case 'a':
-
-        case 'e':
-
-        case 'i':
-
-        case 'o':
-
-        case 'u':
-
-            printf("%c", arr[j]); // Print the vowel if found
-
-            break;
-
-        default:
-
-            break; // Do nothing if the character is not a vowel
-
-        }
-
-    }
-
-    printf("\n");
+    printf("Please Enter 3 integers: ");
 
 
 
-   getchar();
-   getchar();
+
+
+    /*reading the three integers from the user into the integer variables*/
+
+
+
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+
+
+
+
+    /*Calculating average using addition and division operater*/
+
+
+
+    average = (num1 + num2 + num3 ) / 3.0;
+
+
+
+
+
+    //displaying a float with 2 decimal places 
+
+    printf("The average is: %.2f\n", average);
+
+
+
+    getchar();
 
 }
